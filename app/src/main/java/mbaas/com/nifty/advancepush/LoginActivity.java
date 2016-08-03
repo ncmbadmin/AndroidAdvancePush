43b392f25eda90490b6c6374ecb89c7f8dc21b68
ncmbadmin <dinh.thuy.duong@nifty.co.jp>
2016-08-03 12:06:32 +0900

    add login feature

--- app/src/main/java/mbaas/com/nifty/advancepush/LoginActivity.java
+++ app/src/main/java/mbaas/com/nifty/advancepush/LoginActivity.java
@@ -2,8 +2,13 @@ package mbaas.com.nifty.advancepush;
 
 
 
+import com.nifty.cloud.mb.core.LoginCallback;
 import com.nifty.cloud.mb.core.NCMB;
+import com.nifty.cloud.mb.core.NCMBException;
+import com.nifty.cloud.mb.core.NCMBUser;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class
 LoginActivity extends AppCompatActivity {
@@ -19,6 +25,8 @@ LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
+    EditText _loginEmail;
+    EditText _loginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +39,8 @@ LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        EditText _loginEmail = (EditText) findViewById(R.id.txtEmail);
-        EditText _loginPassword = (EditText) findViewById(R.id.txtPassword);
+        _loginEmail = (EditText) findViewById(R.id.txtEmail);
+        _loginPassword = (EditText) findViewById(R.id.txtPassword);
 
         Button _loginButton = (Button) findViewById(R.id.btnLogin);
         Button _signupButton = (Button) findViewById(R.id.btnSignup);
@@ -47,10 +55,49 @@ LoginActivity extends AppCompatActivity {
             }
         });
 
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                doLogin();
+            }
+        });
+
     }
 
     protected void doLogin()
     {
+        String email = _loginEmail.getText().toString();
+        String password = _loginPassword.getText().toString();
+
+        NCMBUser.loginWithMailAddressInBackground(email, password, new LoginCallback() {
+            @Override
+            public void done(NCMBUser user, NCMBException e) {
+                if (e != null) {
+                    //ログインに失敗した場合の処理
+                    new AlertDialog.Builder(LoginActivity.this)
+                            .setTitle("Notification from Nifty")
+                            .setMessage("Login failed! Error:" + e.getMessage())
+                            .setPositiveButton("OK", null)
+                            .show();
+                } else {
+                    //保存成功
+                    new AlertDialog.Builder(LoginActivity.this)
+                            .setTitle("Notification from Nifty")
+                            .setMessage("Login successfull! Please check your mail box.")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //Yesボタンが押された時の処理
+                                    Toast.makeText(LoginActivity.this, "Yes Clicked!", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivityForResult(intent, REQUEST_SIGNUP);
+                                }})
+                            .show();
+
+                }
+            }
+        });
+
 
     }
 
